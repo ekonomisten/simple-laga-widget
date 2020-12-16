4f1624f99df3ade80258418e9f9182d8d19ba3e9
Jakob Bourhil <jakob@genity.se>
2020-12-17 00:54:31 +0100

    Created function for open laga.se in new tab if regno matches Swedish regno, need some fixes tho

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('#lagaWidgetForm');
+const input = document.querySelector('#lagaWidgetRegno');
+const btn = document.querySelector('#lagaWidgetSearch');
+const msg = document.querySelector('.laga-widget__msg');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
+
+btn.addEventListener('click', function() {
+
+    if(!input.value.match(/^[a-öA-Ö]{3}\d{3}$/)) {
+        msg.innerHTML = 'Vänligen fyll i registreringsnummret korrekt.';
+    } else {
+        window.open('https://laga.se/?carbreaker=SB' + input.value);
+        input.value = '';
+    }
+
+})
